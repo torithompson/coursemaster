d61c661bf033685df133683c8edad30999f5b771
Fidy <fidyantenainamanna.fiaferana@student.sl.on.ca>
2023-11-08 16:52:01 -0500

    template added for payments

--- src/main/java/comp31/coursemaster/controllers/TransactionController.java
+++ src/main/java/comp31/coursemaster/controllers/TransactionController.java
@@ -1,5 +1,10 @@
 package comp31.coursemaster.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class TransactionController {
-    
+    @GetMapping("/payment")
+    public String getPayment() {
+        return "payment";
+    }
 }
