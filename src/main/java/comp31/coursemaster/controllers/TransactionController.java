5a01d240f30409967b19772aecc3b43405a297c2
Fidy <fidyantenainamanna.fiaferana@student.sl.on.ca>
2023-11-08 17:00:36 -0500

    Transaction controller more defined

--- src/main/java/comp31/coursemaster/controllers/TransactionController.java
+++ src/main/java/comp31/coursemaster/controllers/TransactionController.java
@@ -1,8 +1,18 @@
 package comp31.coursemaster.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import comp31.coursemaster.services.TransactionService;
+
+@Controller
 public class TransactionController {
+    TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
     @GetMapping("/payment")
     public String getPayment() {
         return "payment";
