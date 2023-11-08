9d05ac5ea11d68e8fbc3324c5e1fbc7b9e1eefd7
trevorwithers <trevor.withers@student.sl.on.ca>
2023-11-08 17:12:32 -0500

    lab 9 commit

--- src/main/java/comp31/coursemaster/model/entities/User.java
+++ src/main/java/comp31/coursemaster/model/entities/User.java
@@ -1,5 +1,48 @@
 package comp31.coursemaster.model.entities;
 
-public class User {
+import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Integer id;
+    String username;
+    String password;
+    List<String> permissions;
+    String firstName;
+    String lastName;
+    String email;
+    String phoneNumber;
+    String address;
+    String city;
+    String province;
+    String postalCode;
+    String country;
+
+    public User(String username, String password, List<String> permissions, String firstName, String lastName,
+            String email, String phoneNumber, String address, String city, String province, String postalCode,
+            String country) {
+        this.username = username;
+        this.password = password;
+        this.permissions = permissions;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.city = city;
+        this.province = province;
+        this.postalCode = postalCode;
+        this.country = country;
+    }
 }
