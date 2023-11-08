276a6df1e7c0963e0719c965f5fc299a300c4415
trevorwithers <trevor.withers@student.sl.on.ca>
2023-11-08 16:43:27 -0500

    the next commit

--- src/main/java/comp31/coursemaster/model/repos/StudentRepo.java
+++ src/main/java/comp31/coursemaster/model/repos/StudentRepo.java
@@ -1,5 +1,12 @@
 package comp31.coursemaster.model.repos;
 
-public class StudentRepo {
+import org.springframework.data.repository.CrudRepository;
 
+import comp31.coursemaster.model.entities.Student;
+
+public interface StudentRepo extends CrudRepository<comp31.coursemaster.model.entities.Student,Integer>{
+    public Student getStudentById(int id);
+    public Student updateStudent(Student student);
+    public int getGradeById(int id);
+    public int getGradesForStudent(int id);
 }
