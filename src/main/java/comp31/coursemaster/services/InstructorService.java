50358a028677ac05b68b15ae55c55175d3ddde33
EthanWatson <ethan.watson2@student.sl.on.ca>
2023-11-08 16:58:21 -0500

    Added to InstructorService

--- src/main/java/comp31/coursemaster/services/InstructorService.java
+++ src/main/java/comp31/coursemaster/services/InstructorService.java
@@ -1,5 +1,30 @@
 package comp31.coursemaster.services;
 
-public class InstructorService {
+import java.util.List;
 
+import comp31.coursemaster.model.entities.Announcement;
+import comp31.coursemaster.model.entities.Assignment;
+import comp31.coursemaster.model.entities.Course;
+import comp31.coursemaster.model.entities.Student;
+
+public class InstructorService {
+    public List<Course> GetAssignedCourses() {
+        return null;
+    }
+
+    public List<Student> GetAssignedStudents() {
+        return null;
+    }
+
+    public List<Announcement> GetSentAnnouncements() {
+        return null;
+    }
+
+    public void CreateAnnouncement() {
+        new Announcement();
+    }
+
+    public void CreateAssignment() {
+        new Assignment();
+    }
 }
