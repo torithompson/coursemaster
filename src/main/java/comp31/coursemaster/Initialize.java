91c36cad5a6bfd97de48c7d24bca3298875fc108
Tori <tori.thompson@student.sl.on.ca>
2023-11-08 16:16:45 -0500

    adding main controller, initialize and loginservice

---
+++ src/main/java/comp31/coursemaster/Initialize.java
@@ -0,0 +1,12 @@
+package comp31.coursemaster;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Initialize implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        // Add your initialization code here
+    }
+}
--- src/main/java/comp31/coursemaster/services/LoginService.java
+++ src/main/java/comp31/coursemaster/services/LoginService.java
@@ -1,5 +1,21 @@
 package comp31.coursemaster.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class LoginService {
 
+    public LoginService() {
+    }
+
+    public String login(String username, String password) {
+        if (username.equals("admin") && password.equals("password"))
+            return "admin";
+        else if (username.equals("student") && password.equals("password"))
+            return "student";
+        else if (username.equals("instructor") && password.equals("password"))
+            return "instructor";
+        else
+            return "invalid";
+    }
 }
