0d20cc69865e3347c59ccfba39a75f589f27376a
Tori <tori.thompson@student.sl.on.ca>
2023-11-08 15:30:54 -0500

    first commit

---
+++ src/main/java/comp31/coursemaster/CoursemasterApplication.java
@@ -0,0 +1,13 @@
+package comp31.coursemaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursemasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursemasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/comp31/coursemaster/CoursemasterApplicationTests.java
@@ -0,0 +1,13 @@
+package comp31.coursemaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursemasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
