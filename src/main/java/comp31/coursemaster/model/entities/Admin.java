caa6c81295c40f9f6e628f254c56e3251a68d97c
Emmaavi <113130478+Emmaavi@users.noreply.github.com>
2023-11-14 14:14:09 -0500

    admin entity

--- src/main/java/comp31/coursemaster/model/entities/Admin.java
+++ src/main/java/comp31/coursemaster/model/entities/Admin.java
@@ -1,5 +1,22 @@
 package comp31.coursemaster.model.entities;
 
-public class Admin {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@NoArgsConstructor
+public class Admin extends User{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Integer id;
+    Integer empNumber;
+
+    public Admin(Integer empNumber) {
+        this.empNumber = empNumber;
+    }
 }
