cc9cd9eddd54a4505d474ece9b2983c72331c71a
EthanWatson <ethanwatson20002@gmail.com>
2023-11-15 17:28:05 -0500

    End of week10 lab

--- src/main/java/comp31/coursemaster/model/entities/Instructor.java
+++ src/main/java/comp31/coursemaster/model/entities/Instructor.java
@@ -1,19 +1,40 @@
 package comp31.coursemaster.model.entities;
 
-import jakarta.persistence.Entity;
+import java.util.List;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "instructor")
+@EqualsAndHashCode(callSuper = false)
 @Data
 @NoArgsConstructor
-@EqualsAndHashCode(callSuper = false)
-public class Instructor {
+public class Instructor extends User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "instructor_id")
     Integer id;
+
+    // @OneToMany(mappedBy = "instructor")
+    // List<Course> courses;
+
+    // @OneToMany(mappedBy = "instructor")
+    // List<Assignment> assignments;
+
+    public Instructor(String username, String password, List<String> permissions,
+            String firstName, String lastName, String email, String phoneNumber,
+            String address, String city, String province, String postalCode,
+            String country /* ,List<Course> courses, List<Assignment> assignments */) {
+        super(username, password, permissions, firstName, lastName, email, phoneNumber,
+                address, city, province, postalCode, country);
+    }
 }
