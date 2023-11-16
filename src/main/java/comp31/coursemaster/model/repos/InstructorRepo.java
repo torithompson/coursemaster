7c3c144aa82ab2afc173f15c2eb05445535ca1f2
Tori <tori.thompson@student.sl.on.ca>
2023-11-15 20:20:33 -0500

    removing list

--- src/main/java/comp31/coursemaster/model/repos/InstructorRepo.java
+++ src/main/java/comp31/coursemaster/model/repos/InstructorRepo.java
@@ -1,10 +1,8 @@
 package comp31.coursemaster.model.repos;
 
-import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
-import comp31.coursemaster.model.entities.Course;
 import comp31.coursemaster.model.entities.Instructor;
 
 public interface InstructorRepo extends CrudRepository<Instructor, Integer> {
