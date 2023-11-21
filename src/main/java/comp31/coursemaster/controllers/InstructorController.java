984ddba978334bff42bee29c2381fe3afd07b2a1
Tori <tori.thompson@student.sl.on.ca>
2023-11-21 10:30:45 -0500

    fixing instructor controller

--- src/main/java/comp31/coursemaster/controllers/InstructorController.java
+++ src/main/java/comp31/coursemaster/controllers/InstructorController.java
@@ -1,10 +1,16 @@
 package comp31.coursemaster.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import comp31.coursemaster.model.entities.Instructor;
+
+@Controller
 public class InstructorController {
     @GetMapping("/instructor")
-    public String getInstructor() {
+    public String getInstructor(Model model) {
+        model.addAttribute("instructor", new Instructor());
         return "instructor";
     }
 
